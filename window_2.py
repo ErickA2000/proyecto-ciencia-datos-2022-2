93da3af2884c2de441110ee86bdb301b27f4dd41
ErickA2000 <erickwladimirarevalonavarro@gmail.com>
2022-12-06 16:09:15 -0500

    funcionando combobox, ya obtiene el valor que se selecciona

--- window_2.py
+++ window_2.py
@@ -6,7 +6,17 @@ def main(df):
     window = Tk()
     columns_name = df.columns.values
     columns_name_list = list(columns_name)
+    frame_1 = Frame( window, bg='gray' )
+    frame_2 = Frame( window, bg="white" )
+
+    combo_1 = ttk.Combobox( frame_1, state="readonly", values=columns_name_list)
+    combo_1.place(x=30, y=30)
+
+    combo_2 = ttk.Combobox( frame_1, state="readonly", values=columns_name_list)
+    combo_2.place(x=280, y=30)
+
     #window_2.mainloop()
+    
     def config( window: Tk ):
         window.config(bg='white')
         window.geometry("600x400")
@@ -17,9 +27,6 @@ def main(df):
         window.rowconfigure(0, weight=25)
         window.columnconfigure(0, weight=1)
         window.rowconfigure(0, weight=1)
-
-        frame_1 = Frame( window, bg='gray' )
-        frame_2 = Frame( window, bg="white" )
 
         frame_1.grid(column=0, row=0, sticky='nsew')
         
@@ -39,17 +46,19 @@ def main(df):
         frame_2.columnconfigure(3, weight=2)
         frame_2.rowconfigure(0, weight=1)
 
-        configFrame( frame_1, frame_2 )
+        configFrame( )
 
-    def showSelection( combo_1: ttk.Combobox, combo_2: ttk.Combobox ):
+    def showSelection( ):
         selection_1 = combo_1.get()
         selection_2 = combo_2.get()
         print( selection_1, selection_2 )
+        
 
-    def configFrame( frame_1: Frame, frame_2: Frame ):
-        combo_1 = ttk.Combobox( frame_1, state="readonly", values=columns_name_list).place(x=30, y=30)
-        combo_2 = ttk.Combobox( frame_1, state="readonly", values=columns_name_list).place(x=280, y=30)
+    def configFrame( ):
+        
         #Frame 2
-        Button( frame_2, text="Prueba", command=lambda:[showSelection(combo_1, combo_2)] ).pack()
+        Button( frame_2, text="Prueba", command=showSelection ).pack()
+
+    
 
     config( window )
